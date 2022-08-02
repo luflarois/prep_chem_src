890d845db030e6df8265be8da5200ddb7db3a641
luflarois <luflarois@gmail.com>
2022-08-02 15:06:05 -0300

    Versão Inicial
    Essa é a versão baseada no status em que está o código disponível em https://svn.cptec.inpe.br/prep-chem-src/tags/PREP-CHEM-SRC-1.8.3
    na data de 02Ago2022

---
+++ bin/build/paths.mk
@@ -0,0 +1,13 @@
+#Makefile include paths.mk
+
+#Prepsource related
+
+LIB_RAMS_PATH =../../aux_src/utils
+RAMS_PATH     =../../aux_src/brams
+PREPSOURCE_SRC=../../src
+LIB_WPS_PATH  =../../aux_src/wps
+
+AER_DIR=$(AER)
+ifeq ($(CHEM), RADM_WRF_FIM)
+AER_DIR=SIMPLE
+endif
